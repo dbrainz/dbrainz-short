c9b192f74c03ad9b43b63d00ba762457cfa1b891
Darren Brain <darren.brain@gmail.com>
2017-04-07 18:26:44 +0000

    server.js revisions left out of previous commit

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ var validUrl = require('valid-url');
 var app = express()
 var mongoURL = "mongodb://dbrainzfcc:shortener@ds133428.mlab.com:33428/shorten"
 
+app.get('/', express.static('.'))
 app.get('/*', (req,res) => {
     if (req.params[0].substr(0,4) == "new/") {
         var longUrl = req.params[0].substr(4)
-        if (!(validUrl.isHttpUri(longUrl))) {
+        if (!(validUrl.isUri(longUrl))) {
+            console.log(longUrl);
             console.log('invalid url');
             res.end(JSON.stringify({'error' : 'invalid url'}))
             return
@@ -19,7 +21,7 @@ app.get('/*', (req,res) => {
     
             db.collection('urls').find({}).sort({$natural:-1}).limit(1).toArray((err, documents) => {
                 if (err) throw err;
-                var newKey = documents[0].url_key + 1;
+                var newKey = Number(documents[0].url_key) + 1;
                 var collection = db.collection('urls');
                 var insertRec = {'url_key' :  newKey, 'url' : longUrl}
                 collection.insert( insertRec, (err, data) => { 
@@ -33,25 +35,28 @@ app.get('/*', (req,res) => {
 
         })
     }
-    else {
-        if (req.params[0].length > 0) {
-            mongo.connect(mongoURL, (err, db) => {
-                if (err) throw err;
+    else if (req.params[0].length > 0) {
+        mongo.connect(mongoURL, (err, db) => {
+            if (err) throw err;
                 
-                var collection = db.collection('urls');
+            var collection = db.collection('urls');
 
-                collection.findOne({url_key:  Number(req.params[0])}, function(err, document) {
-                    if (err) throw err;
-                    if (document != null) {
-                        console.log(document.url_key);
-                    }
-                    else {
-                        console.log('no record found');
-                    }
-                });
-            })
-        }
+            collection.findOne({url_key:  Number(req.params[0])}, function(err, document) {
+                if (err) throw err;
+                if (document != null) {
+                    res.redirect(document.url);
+                }
+                else {
+                    res.end(JSON.stringify({ 'error' : 'url not in database'}));
+                }
+            });
+        })
+        
     }
+    else {
+        express.static("index.html");
+    }
+    
 })
 
 app.listen(process.env.PORT || 3000 || 8080);
